7408916a33bd1f7413a249340e9c361757153515
DanyelleCorreia <Danyelle031199@gmail.com>
2025-09-08 23:49:42 -0300

    Update pesq.py

--- src/utils/pesq.py
+++ src/utils/pesq.py
@@ -1,1 +1,13 @@
 #Danielly
+def search_planet(planets):
+    termo = input("Digite o nome do planeta para buscar: ").strip().lower()
+    encontrados = [p for p in planets if termo in p['nome'].lower()]
+
+    if not encontrados:
+        print("🔎 Nenhum planeta encontrado com esse nome.\n")
+        return
+
+    print("\n--- Resultados da Pesquisa ---")
+    for planet in encontrados:
+        print(f"ID: {planet['id']} | Nome: {planet['nome']} | Tamanho: {planet['tamanho']} km | Habitável: {planet['habitavel']}")
+    print()
