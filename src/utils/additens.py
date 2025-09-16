83865ceca4195b7e2e9924fbb3478e31ce6c5829
Lorenadalmeida <llorenaliv07@gmail.com>
2025-09-16 23:38:54 +0000

    inicio da mudança de nome: adicionar item

--- src/utils/additens.py
+++ src/utils/additens.py
@@ -1,1 +1,17 @@
 #Lorena
+# planetas = [] 
+def adicionar_eventos():
+    nome = input("digite o nome do planeta:" )
+    tamanho = float(input("digite o tamanho do planeta (em km):"))
+    distancia = float(input("digite a distancia do sol (em milhões de km):"))
+
+    planeta = {
+        "nome": nome,
+        "tamanho": tamanho,
+        "distancia": distancia 
+        }  
+    eventos.append(planeta)
+    print(f"\n evento '{ }) registrado com sucesso!")
+    print(adicionar_eventos)
+
+adicionar_eventos()
