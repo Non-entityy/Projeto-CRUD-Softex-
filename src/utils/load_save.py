033aefe67f99fb81d4c9f0ded8a74a47bceab552
rodrigopuma <rodrigopuma.dev@outlook.com>
2025-09-16 19:41:58 -0300

    fix(data): - Corrigindo o caminho do arquivo

--- src/utils/load_save.py
+++ src/utils/load_save.py
@@ -3,17 +3,22 @@ from models.event import Evento
 #from models.event import Evento
 #from utils.load_save import carregar_eventos, salvar_eventos
 
+PATH = "src/data/events.json" # Executar no diretorio principal
+
 # Função para carregar eventos do arquivo JSON
 def carregar_eventos():
     try:
-        with open('events.json', 'r') as f:
-            dados = json.load(f)
-            return [Evento(**evento) for evento in dados]  # Converte dicionários em objetos Evento
+        with open(PATH, 'r') as f:
+            dados = list(json.load(f))
+            return dados
+            # return [Evento(**evento) for evento in dados]  # Converte dicionários em objetos Evento
     except FileNotFoundError:
+        return []
+    except TypeError:
         return []
 
 # Função para salvar os eventos no arquivo JSON
 def salvar_eventos(eventos):
     eventos_dict = [evento.to_dict() for evento in eventos]  # Converte objetos Evento em dicionários
-    with open('events.json', 'w') as f:
+    with open(PATH, 'w') as f:
         json.dump(eventos_dict, f, indent=4)
