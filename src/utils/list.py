62185dc606c78711b13a7dbeedda4077d63a523c
rodrigopuma <rodrigopuma.dev@outlook.com>
2025-09-16 21:15:14 -0300

    feat(edit, list):
    - Criando a função de "editar_item()"
    
    - Incrementando um parametro bool para ver se o usuario deseja exibir os eventos com id para uma funcionalidade dentro da função

--- src/utils/list.py
+++ src/utils/list.py
@@ -1,8 +1,13 @@
 from load_save import carregar_eventos
 
-def list_events():
+def list_events(exibir_id: bool):
     events = carregar_eventos()
     for indice, evento in enumerate(events):
         print(f"Evento ID {indice}:")
         for dados, informacao in evento.items():
+            if exibir_id:
+                pass
+            else:
+                if dados == "id":
+                    continue
             print(f"    {dados}: {informacao}")
