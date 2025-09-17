06bb9606aff78180e1447b018a2fd0c741c9e7fc
Lorenadalmeida <llorenaliv07@gmail.com>
2025-09-17 00:08:29 +0000

    alteracao

--- src/utils/additens.py
+++ src/utils/additens.py
@@ -1,22 +1,16 @@
 #Lorena
 # atributos = [] nome, data, local, descricao, tipo
-def create_eventos():
-   evento = float(input("nome do evento:"))
-    data = float(input("data do evento:"))
-    local = float(input("local do evento:"))
-    descricao = float(input("Descrição do evento"))
-    tipo = float(input("tipo de evento:"))
+from models.event import Evento
+def create_eventos(self,eventos):
+   self.nome = float(input("nome do evento:"))
+   self.data = float(input("data do evento:"))
+   self.local = float(input("local do evento:"))
+   self.descricao = float(input("Descrição do evento"))
+   self.tipo = float(input("tipo de evento:"))
 
-    evento = {
-        "evento": evento,
-        "data": data,
-        "local": local,
-        "descricao": descricao,
-        "tipo": tipo,
-        }  
-    eventos.append(evento)
-    print(f"\n evento '{ }) registrado com sucesso!")
-    print(create_eventos)
+   eventos.append(eventos)
+print(f"\n evento {nome} registrado com sucesso!")
+print(create_eventos)
 
 create_eventos()
 
