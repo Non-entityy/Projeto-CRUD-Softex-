c96fb50c53f5017181bd5eef678d2b81b6e36b47
rodrigopuma <rodrigopuma.dev@outlook.com>
2025-09-18 20:42:02 -0300

    feat(exibir_id): - Implementando funções na exibição de ID

--- src/utils/list.py
+++ src/utils/list.py
@@ -1,13 +1,16 @@
-from load_save import carregar_eventos
+from utils.load_save import carregar_eventos
 
-def list_events(exibir_id: bool):
+def list_events(exibir_id: bool= True):
     events = carregar_eventos()
-    for indice, evento in enumerate(events):
-        print(f"Evento ID {indice}:")
+    for indice, evento in enumerate(events, start=1):
+        print(f"Evento ID {indice}:") if not exibir_id else None
         for dados, informacao in evento.items():
             if exibir_id:
                 pass
             else:
                 if dados == "id":
                     continue
-            print(f"    {dados}: {informacao}")
+            
+            if exibir_id: print(f"{dados}: {informacao}")
+
+            if not exibir_id: print(f"    {dados}: {informacao}")
