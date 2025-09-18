11cb4a521cb07497c9d777a6cea20dbe055fa264
Lorenadalmeida <llorenaliv07@gmail.com>
2025-09-18 21:53:21 +0000

    .

--- src/utils/additens.py
+++ src/utils/additens.py
@@ -1,6 +1,6 @@
 # #Lorena
 # # atributos = [] nome, data, local, descricao, tipo
-def adicionar_evento(eventos):
+def adicionar_evento(evento):
     print("\nAdicionando um novo evento:")
     nome = input("Nome do Evento: ")
     data = input("Data do Evento (AAAA-MM-DD): ")
@@ -8,8 +8,7 @@ def adicionar_evento(eventos):
     descricao = input("Descrição do Evento: ")
     tipo = input("Tipo do Evento: ")
 
-    novo_evento = Evento(nome, data, local, descricao, tipo)
-    
-    eventos.append(novo_evento)
-    salvar_eventos(evento)
+    novo_evento = evento(nome, data, local, descricao, tipo)
+    evento.append(novo_evento)
+    salvar_evento = (evento)
     print("Evento adicionado com sucesso!")
