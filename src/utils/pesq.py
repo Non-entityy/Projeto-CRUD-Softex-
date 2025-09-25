dfbe9406ee21bd469b886aa56971d98bd054ec47
Danyelle <danyelle031199@gmail.com>
2025-09-25 20:03:11 -0300

    pesquisa

--- src/utils/pesq.py
+++ src/utils/pesq.py
@@ -1,9 +1,8 @@
 #Danyelle
+from utils.load_save import carregar_eventos
+
+
 # input 
-
-
-
-
 # def search_eventos (Evento):
 #     termo = input("Digite o nome do evento para buscar: ").strip().lower()
 #     encontrados = [p for p in Evento if termo in p['nome'].lower()]
@@ -17,25 +16,32 @@
 #         print(f"ID: {Evento['id']} | Nome: {Evento['nome']} | Local: {Evento['local']} | Descrição: {Evento['descrição']} | Tipo: {Evento['tipo']}")
 #     print()
 
-from utils.load_save import carregar_eventos
+# 
 
 def search_event(events=None):
     """Pesquisa eventos pelo nome (case insensitive)."""
     if events is None:
         events = carregar_eventos()
 
-    termo = input("Digite o nome (ou parte) do evento: ").strip().lower()
-    encontrados = [evento for evento in events if termo in evento["nome"].lower()]
+    termo = input("Digite o nome (ou id) do evento: ").strip().lower()
+    encontrados = []
 
+    for evento in events:
+        nome= evento["nome"].lower()
+        id_str =str (evento["id"])
+        if termo in nome or termo == id_str:
+            encontrados.append(evento)
     if not encontrados:
         print("\nNenhum evento encontrado.\n")
+       
         return []
 
     print("\n--- Resultados da Pesquisa ---")
-    for idx, evento in enumerate(encontrados, start=1):
+    for idx, evento in enumerate(encontrados):
         print(f"\nResultado {idx}:")
         for chave, valor in evento.items():
             print(f"  {chave}: {valor}")
+            
     print()
 
     return encontrados
