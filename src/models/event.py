f7e09ce2b668a3910fd7b35439e0b137fbe009c6
rodrigopuma <rodrigopumalima@gmail.com>
2025-10-10 00:46:29 -0300

    feat(event): Argumentos novos da classe - Quantidade de Ingressos Disponiveis - Idade Minima para acessar o evento

--- src/models/event.py
+++ src/models/event.py
@@ -1,11 +1,16 @@
 class Evento:
-    def __init__(self, nome, data, local, descricao, tipo):
+    def __init__(
+            self, nome: str, data: str, 
+            local: str, descricao: str, tipo: str, 
+            qtd_ingressos_disponiveis: int, idade_minima: int):
         self.id = id((nome, data, local, descricao, tipo)) # O id pode ser uma tupla do proprio objeto
         self.nome = nome
         self.data = data
         self.local = local
         self.descricao = descricao
         self.tipo = tipo
+        self.qtd_ingressos_disponiveis = qtd_ingressos_disponiveis
+        self.idade_minima = idade_minima
         self.status = True # True para ativo | False para inativo
 
     def __repr__(self):
@@ -18,6 +23,5 @@ class Evento:
             "nome": self.nome,
             "data": self.data,
             "local": self.local,
-            "descricao": self.descricao,
-            
+            "descricao": self.descricao,   
         }
