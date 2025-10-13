387e9a3f36e2e06acb28b8ab88bf09791e402c9f
rodrigopuma <rodrigopumalima@gmail.com>
2025-10-13 17:42:49 -0300

    refactor(load_save): Atualizando e unificando funções de carregar e salvar JSON

--- src/utils/load_save.py
+++ src/utils/load_save.py
@@ -1,80 +1,38 @@
 import json
+import os
 
-PATH_EVENTOS = "src/data/events.json" # Executar no diretorio principal
-PATH_INGRESSOS = "src/data/ingress.json"
-PATH_EMPRESA = "src/data/enterprises.json"
-PATH_PESSOA = "src/data/persons.json"
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-def carregar_eventos():
+def get_file_path(categoria):
+    nome_arquivo = categoria.lower() + 's.json'
+    caminho = os.path.join(BASE_DIR, nome_arquivo)
+    return caminho
+
+def load_data(categoria) -> list:
     """Carrega a lista de eventos do arquivo JSON principal
 
-    Returns:
-        list: Se encontrar o arquivo retornara a lista com os eventos, se não retornara uma lista vazia
-    """
-    try:
-        with open(PATH_EVENTOS, 'r', encoding="utf-8") as f:
-            dados = json.load(f)
-            return dados
-            # return [Evento(**evento) for evento in eventos_json if not evento["id"]]  # Converte dicionários em objetos Evento
-    except FileNotFoundError:
-        return []
-    except TypeError:
-        return []
-
-def salvar_eventos(eventos):
-    """Salva a lista completa com eventos no arquivo JSON
-
     Args:
-        eventos (list): Lista com todos os eventos que vão ser salvos no JSON (Incluindo os já existentes)
-    """
-    eventos_dict = [evento for evento in eventos]  # Converte objetos Evento em dicionários
-    with open(PATH_EVENTOS, 'w', encoding="utf-8") as f:
-        json.dump(eventos_dict, f, indent=4, ensure_ascii=False)
-        
-def carregar_ingressos():
-    """Carrega a lista de ingressos do arquivo JSON principal
+        categoria (str): Categoria do arquivo ex: event → events.json
 
     Returns:
-        list: Se encontrar o arquivo retornara a lista com os ingressos, se não retornara uma lista vazia
+        list: Se encontrar o arquivo retornara a lista com os dados, se não retornara uma lista vazia
     """
-    try:
-        with open(PATH_INGRESSOS, 'r', encoding="utf-8") as f:
+    caminho = get_file_path(categoria)
+    if not os.path.exists(caminho):
+        return []
+    with open(caminho, 'r', encoding='utf-8') as f:
+        try:
             return json.load(f)
-    except FileNotFoundError:
-        return []
-    except TypeError:
-        return []
+        except json.JSONDecodeError:
+            return []
 
-def salvar_ingressos(ingressos):
-    """Salva a lista completa com ingressos no arquivo JSON
+def save_data(categoria, dados):
+    """Salva a lista completa com dados no arquivo JSON
 
     Args:
-        ingressos (list): Lista com todos os ingressos que vão ser salvos no JSON (Incluindo os já existentes)
+        categoria (str): Categoria do arquivo ex: event → events.json
+        dados (list): Lista com todos os eventos que vão ser salvos no JSON (Incluindo os já existentes)
     """
-    ingressos_dict = [ingresso for ingresso in ingressos]
-    with open(PATH_INGRESSOS, 'w', encoding="utf-8") as f:
-        json.dump(ingressos_dict, f, indent=4, ensure_ascii=False)
-        
-def carregar_ingressos():
-    """Carrega a lista de ingressos do arquivo JSON principal
-
-    Returns:
-        list: Se encontrar o arquivo retornara a lista com os ingressos, se não retornara uma lista vazia
-    """
-    try:
-        with open(PATH_INGRESSOS, 'r', encoding="utf-8") as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return []
-    except TypeError:
-        return []
-
-def salvar_ingressos(ingressos):
-    """Salva a lista completa com ingressos no arquivo JSON
-
-    Args:
-        ingressos (list): Lista com todos os ingressos que vão ser salvos no JSON (Incluindo os já existentes)
-    """
-    ingressos_dict = [ingresso for ingresso in ingressos]
-    with open(PATH_INGRESSOS, 'w', encoding="utf-8") as f:
-        json.dump(ingressos_dict, f, indent=4, ensure_ascii=False)
+    caminho = get_file_path(categoria)
+    with open(caminho, 'w', encoding='utf-8') as f:
+        json.dump(dados, f, indent=2, ensure_ascii=False)
