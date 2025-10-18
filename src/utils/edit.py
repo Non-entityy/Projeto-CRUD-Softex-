0171906842bb5b7b3ae997e9b1359699cec2148d
rodrigopuma <rodrigopumalima@gmail.com>
2025-10-18 11:19:25 -0300

    docs(edit): Antencipando funcionalidade de trabalho com objetos

--- src/utils/edit.py
+++ src/utils/edit.py
@@ -1,4 +1,5 @@
 #Rodrigo
+import inspect
 from utils.load_save import load_data, save_data
 from utils.list import list_events
 from utils.functions import try_input_int
@@ -53,3 +54,46 @@ def editar_item():
     events.insert(indice_item - 1, evento_selecionado) # Salvando evento atualizado removendo a informação antiga e adicionando a nova
 
     save_data("event", events) # Salvando a lista de eventos diretamente no arquivo JSON
+
+"""Futura implementação POO para o DB
+def editar_item_obj(cls):
+    # cls.__name__ -> retorna a classe
+    # ------------[ Obtém os parâmetros do construtor da classe (__init__) ]------------
+    parametros_cls = inspect.signature(cls.__init__).parameters
+    args_necessarios = [p for p in parametros_cls if p != "self"] # Ignora o primeiro parâmetro 'self'
+    
+    lists = events_json
+    
+    print(f"Carregando todos os {cls.__name__}s..."), sleep(1.5) # EXIBIÇÃO
+    list_events(exibir_id=False), sleep(1) # EXIBIÇÃO
+
+    indice_item = try_input_int("Digite o ID do item: ") # Não altera durante a execução é o ID do item dentro da lista
+
+    while True:
+        try:    
+            print(f"Carregando {cls.__name__} de ID {indice_item}..."), sleep(1.5)
+            dicionario_selecionado = dict(lists[indice_item - 1])
+        except IndexError:
+            indice_item = try_input_int("Esse ID não foi encontrada verifique se digitou corretamente e tente novamente: ")
+        else:
+            dicionario_selecionado = dict(lists[indice_item - 1])
+            break
+    
+    for indice, atributo, valor in enumerate(dicionario_selecionado.items(), start=1):
+        if atributo == "id":
+            continue
+        
+        print(f"[{indice}] -> {atributo}: {valor}")
+    
+    indice_atributo = try_input_int("Digite o ID da informação que deseja alterar: ")
+    
+    valores_existentes = [valor for valor in dicionario_selecionado.values()]
+    valores = {}
+    for i, arg in enumerate(args_necessarios):
+        valor = valores_existentes[i]
+        valores[arg] = valor
+    
+    instancia = cls(**valores)
+    
+    instancia.edit(indice_atributo)
+"""
